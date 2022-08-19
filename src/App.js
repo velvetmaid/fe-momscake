a068b05ab30759a5bea9a1cb6164b68e8132eb7a
CiuusBandicoot <tarsisiusadhi@gmail.com>
2022-08-19 11:38:46 +0700

    TEMP

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 import "./style/landingPage.css";
 import "./fonts/Ruthie-Regular.ttf";
-
+//https://pandorcakes.com/
 function App() {
   return (
-    <div className="jumbotorn">
-      <NavBar />
-      <Intro />
-    </div>
+    <>
+      <div className="navInt">
+        <NavBar />
+        <Intro />
+      </div>
+    </>
   );
 }
 
